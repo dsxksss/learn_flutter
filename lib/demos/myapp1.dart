7d35ef40a42f7a84a87398369d77856b6d91d1bd
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-04-29 16:50:00 +0800

    add:demo1基本布局内容完结

--- lib/demos/myapp1.dart
+++ lib/demos/myapp1.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var currentIndex = 0;
+  String buttonName = "abc";
+
+  void changeButton() => setState(() {
+        buttonName = "xawdaw";
+      });
 
   @override
   Widget build(BuildContext context) => (MaterialApp(
@@ -10,67 +22,54 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: const Text("App Title"),
           ),
-          body: const Body(),
-          bottomNavigationBar: const ButtonNavigationBar(),
+          body: Center(
+            //Container组件和SizedBox组件特别相像，
+            //除了Container组件可以增加color背景属性
+            child: currentIndex == 0
+                ? Container(
+                    //设置屏幕的最大宽占比
+                    width: double.infinity,
+                    height: double.infinity,
+                    color: Colors.blueGrey,
+                    child: Row(
+                      //这个属性控制垂直排列的方式
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      //这个属性控制水平排列的方式
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            //文字颜色
+                            onPrimary: Colors.white,
+                            //按钮背景颜色
+                            primary: Colors.red,
+                          ),
+                          onPressed: changeButton,
+                          child: Text(buttonName),
+                        ),
+                        ElevatedButton(
+                          onPressed: changeButton,
+                          child: Text(buttonName),
+                        ),
+                      ],
+                    ),
+                  )
+                : Image.network(
+                    "https://images.pexels.com/photos/8965700/pexels-photo-8965700.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings), label: 'Settings'),
+            ],
+            currentIndex: currentIndex,
+            onTap: (int index) {
+              setState(() {
+                currentIndex = index;
+              });
+            },
+          ),
         ),
       ));
-}
-
-class Body extends StatefulWidget {
-  const Body({Key? key}) : super(key: key);
-
-  @override
-  State<Body> createState() => _BodyState();
-}
-
-class _BodyState extends State<Body> {
-  String buttonName = "abc";
-
-  void changeButton() => setState(() {
-        buttonName = "xawdaw";
-      });
-
-  @override
-  Widget build(BuildContext context) => (Center(
-          child: Column(
-        //这个属性控制垂直排列的方式
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: changeButton,
-            child: Text(buttonName),
-          ),
-          ElevatedButton(
-            onPressed: changeButton,
-            child: Text(buttonName),
-          ),
-        ],
-      )));
-}
-
-class ButtonNavigationBar extends StatefulWidget {
-  const ButtonNavigationBar({Key? key}) : super(key: key);
-
-  @override
-  State<ButtonNavigationBar> createState() => _ButtonNavigationBarState();
-}
-
-class _ButtonNavigationBarState extends State<ButtonNavigationBar> {
-  int currentIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
-      ],
-      currentIndex: currentIndex,
-      onTap: (int index) {
-        setState(() {
-          currentIndex = index;
-        });
-      },
-    );
-  }
 }
